7fd333d54e364ac7540cd305ef430c599b6e5550
EricHindle <ehindle@netwyrks.co.uk>
2013-08-08 12:55:11 +0100

    New source

---
+++ cbdescupdate/cbdescupdate/frmCbDesc.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCbDesc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.txtTitle = New System.Windows.Forms.TextBox
+        Me.txtCaption = New System.Windows.Forms.TextBox
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown
+        Me.txtUrl = New System.Windows.Forms.TextBox
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 447)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(562, 108)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(495, 570)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(81, 31)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 570)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(81, 31)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Get Desc"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(98, 374)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(478, 20)
+        Me.txtTitle.TabIndex = 3
+        '
+        'txtCaption
+        '
+        Me.txtCaption.Location = New System.Drawing.Point(98, 411)
+        Me.txtCaption.Name = "txtCaption"
+        Me.txtCaption.Size = New System.Drawing.Size(478, 20)
+        Me.txtCaption.TabIndex = 4
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"})
+        Me.ComboBox1.Location = New System.Drawing.Point(19, 30)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(63, 26)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"})
+        Me.ComboBox2.Location = New System.Drawing.Point(127, 30)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(203, 26)
+        Me.ComboBox2.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(387, 28)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(81, 31)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Get Date"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(96, 77)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(478, 21)
+        Me.ComboBox3.TabIndex = 8
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(96, 105)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(765, 263)
+        Me.WebBrowser1.TabIndex = 9
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(19, 78)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {31, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(63, 20)
+        Me.NumericUpDown1.TabIndex = 10
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {25, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(19, 105)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {12, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(63, 20)
+        Me.NumericUpDown2.TabIndex = 11
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {3, 0, 0, 0})
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(19, 131)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {2015, 0, 0, 0})
+        Me.NumericUpDown3.Minimum = New Decimal(New Integer() {2010, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(63, 20)
+        Me.NumericUpDown3.TabIndex = 12
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {2013, 0, 0, 0})
+        '
+        'txtUrl
+        '
+        Me.txtUrl.Location = New System.Drawing.Point(495, 36)
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(366, 20)
+        Me.txtUrl.TabIndex = 13
+        '
+        'frmCbDesc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(873, 613)
+        Me.Controls.Add(Me.txtUrl)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.txtCaption)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "frmCbDesc"
+        Me.Text = "CB Description"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents txtTitle As System.Windows.Forms.TextBox
+    Friend WithEvents txtCaption As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents txtUrl As System.Windows.Forms.TextBox
+
+End Class
---
+++ cbdescupdate/cbdescupdate/frmCbDesc.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Text
+Imports System.IO
+
+Public Class frmCbDesc
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim sUrl As New StringBuilder("http://celebritybirthday.wordpress.com/")
+        sUrl.Append(Format(NumericUpDown3.Value, "0000"))
+        sUrl.Append("/")
+        sUrl.Append(Format(NumericUpDown2.Value, "00"))
+        sUrl.Append("/")
+        sUrl.Append(Format(NumericUpDown1.Value, "00"))
+        sUrl.Append("/")
+        sUrl.Append(ComboBox1.Text)
+        sUrl.Append("-")
+        sUrl.Append(ComboBox2.Text)
+        sUrl.Append("/")
+        txtUrl.Text = sUrl.ToString
+        WebBrowser1.Navigate(sUrl.ToString)
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        Dim sDtElements As HtmlElementCollection = WebBrowser1.Document.GetElementsByTagName("dt")
+        ComboBox3.Items.Clear()
+        RichTextBox1.Text = ""
+        For Each sDtElement As HtmlElement In sDtElements
+            Dim sElements As HtmlElementCollection = sDtElement.GetElementsByTagName("a")
+            For Each sElement As HtmlElement In sElements
+                Dim sTitle As String = sElement.GetAttribute("title")
+                Dim sHref As String = sElement.GetAttribute("href")
+                ComboBox3.Items.Add(sTitle)
+
+
+                Dim Client As New System.Net.WebClient
+                Dim strm As String = Client.DownloadString(sHref)
+                Dim srch As String = "<div class=""entry-caption"">"
+                If strm.Contains(srch) Then
+                    Dim start As Int16 = strm.IndexOf(srch) + srch.Length + 3
+
+                    Dim newstart As Integer = strm.IndexOf("<p>", start) + 3
+                    Dim newend As Int16 = strm.IndexOf("</p>", newstart)
+
+                    Dim desc As String = strm.Substring(newstart, newend - newstart)
+                    RichTextBox1.Text = RichTextBox1.Text & desc & vbCrLf
+                End If
+
+
+
+            Next
+        Next
+    End Sub
+
+End Class
