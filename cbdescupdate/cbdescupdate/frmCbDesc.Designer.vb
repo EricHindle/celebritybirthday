cb5ed2a8447373c5769dd5ffd2ca281a05d22942
EricHindle <ehindle@netwyrks.co.uk>
2013-08-09 16:02:20 +0100

    Blue background
    Signed-off-by: EricHindle <ehindle@netwyrks.co.uk>

--- cbdescupdate/cbdescupdate/frmCbDesc.Designer.vb
+++ cbdescupdate/cbdescupdate/frmCbDesc.Designer.vb
@@ -177,6 +177,7 @@ Partial Class frmCbDesc
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSteelBlue
         Me.ClientSize = New System.Drawing.Size(537, 482)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtUrl)
